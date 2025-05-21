6013a31f3f9d660e5ed2dc604c76bab47fffb896
muhammadalfayed <m.alfayed23si@mahasiswa.pcr.ac.id>
2025-05-21 15:41:21 +0700

    Saya sudah selesai buat product,penjualan crud

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route } from 'react-router-dom'
 import MainLayout from './components/MainLayout'
 import Dashboard from './pages/Dashboard'
+import ProductManagement from './pages/ProductManagement'
+import SalesManagement from './pages/SalesManagement'
 
 function App() {
 
@@ -8,6 +10,8 @@ function App() {
     <Routes>
       <Route element={<MainLayout/>}>
       <Route path="/" element={<Dashboard/>}/>
+      <Route path="/penjualan" element={<SalesManagement/>}/>
+      <Route path="/produk" element={<ProductManagement/>}/>
       </Route>
     </Routes>
   )
