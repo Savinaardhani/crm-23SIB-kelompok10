39b8f0f006703d3d52e0b03296b052e0fd669df3
Savina Aulia <savina23si@mahasiswa.pcr.ac.id>
2025-05-28 16:51:15 +0700

    ok fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import MainLayout from './components/MainLayout'
 import Dashboard from './pages/Dashboard'
 import CustomerManagement from './pages/CustomerManagement'
 import ProductManagement from './pages/ProductManagement'
-import SalesManagement from './pages/SalesManagement'
 import FeedbackReview from './pages/FeedbackReview'
 import LoyaltyReward from './pages/LoyaltyReward'
 
@@ -18,7 +17,6 @@ function App() {
       <Route element={<MainLayout/>}>
       <Route path="/" element={<Dashboard/>}/>
       <Route path="/pelanggan" element={<CustomerManagement/>}/>
-      <Route path="/penjualan" element={<SalesManagement/>}/>
       <Route path="/Order" element={<Order/>}/>
       <Route path="/produk" element={<ProductManagement/>}/>
       <Route path="/review" element={<FeedbackReview />} />
